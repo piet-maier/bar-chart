763ddce705cc7e60ad46066308a5042dce5899cb
Piet Maier <pietmaier.github@protonmail.com>
2025-07-04 19:53:05 +0200

    Add Build Configuration

---
+++ lib/main.ts
@@ -0,0 +1,1 @@
+export { default as BarChart } from "../src/components/BarChart.vue";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import vue from "@vitejs/plugin-vue";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ["d3", "vue"],
+      output: {
+        globals: {
+          d3: "d3",
+          vue: "Vue",
+        },
+      },
+    },
+    lib: {
+      entry: resolve(dirname(fileURLToPath(import.meta.url)), "lib/main.ts"),
+      name: "BarChart",
+      fileName: "bar-chart",
+    },
+    copyPublicDir: false,
+  },
 });
